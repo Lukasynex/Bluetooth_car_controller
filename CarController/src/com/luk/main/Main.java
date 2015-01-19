594dde2ca711427f4ce901f4c7b60a8c094aad31
lukmar993 <lukmar993@gmail.com>
2015-01-19 01:14:59 +0100

    action pointer up

--- CarController/src/com/luk/main/Main.java
+++ CarController/src/com/luk/main/Main.java
@@ -30,10 +30,35 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+/**
+ * projekt gotowy, ewentualnie można dopieścić usuwając wcięcia, komentarze, zmienne, 
+ * czy też nieużywane sensory położenia
+ * 
+ * @author lukasz
+ *
+ */
+
 @SuppressLint("ClickableViewAccessibility")
 public class Main extends Activity implements SensorEventListener {
 	private boolean LIGHT = false;
-	private boolean SOUND = false;
+	//these variables could be programmed
+	public char global_UP = '0';
+	public char global_RIGHT = '1';
+	public char global_DOWN = '2';
+	public char global_LEFT = '3';
+	
+	public char global_UP_DISABLE = '4';
+	public char global_RIGHT_DISABLE = '5';
+	public char global_DOWN_DISABLE = '6';
+	public char global_LEFT_DISABLE = '7';
+	
+	public char global_INCREASE_SPEED = 'w';
+	public char global_DECREASE_SPEED = 's';
+	public char global_SWITCH_LIGHTS = 'a';
+	public char global_SOUND_ON = 'k';
+	public char global_SOUND_OFF = 'n';
+	
+	public boolean ScreenAceII = true;
 	
 	// kod przekazania adresu urzadzenia
 	private static final int GET_DEVICE = 1;
@@ -47,6 +72,7 @@ public class Main extends Activity implements SensorEventListener {
 
 	// bluetooth
 	private BluetoothAdapter mBluetoothAdapter;
+	//private int LastMultiState = -1;
 	private BluetoothDevice mDevice;
 	private BluetoothSocket mSocket;
 	private static final UUID uuid = UUID
@@ -61,7 +87,6 @@ public class Main extends Activity implements SensorEventListener {
 
 	// kontrolki
 	private TextView mTextState;
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -98,7 +123,9 @@ public class Main extends Activity implements SensorEventListener {
 
 	private boolean ClickedButton(float dx, float dy,
 			int clicked) {
-
+		
+		
+		if(ScreenAceII){
 		switch(clicked){
 			case 0:{//up
 				if(dx < 200.0F && dy < 200.0F)
@@ -120,26 +147,70 @@ public class Main extends Activity implements SensorEventListener {
 					return true;
 				else return false;
 			}
-			case 4:{
+			case 4:{//sound
 				if(dx>200 && dx<400 && dy<200)
 					return true;
 				else return false;
 			}
-			case 5:{
+			case 5:{//lights
 				if(dx>200 && dx<400 && dy>200)
 					return true;
 				else return false;
 			}
-			case 6:{
+			case 6:{//acc++
 				if(dx>400 && dx<700 && dy<200)
 					return true;
 				else return false;
 			}
-			case 7:{
+			case 7:{//acc--
 				if(dx>400 && dx<700 && dy>200)
 					return true;
 				else return false;
 			}
+		}}
+		else{
+			switch(clicked){
+			case 0:{//up
+				if(dx < 120 && dy < 160.0F)
+					return true;
+				else return false;
+			}
+			case 1:{//left
+				if(dx > 360 && dy < 160.0F)
+					return true;
+				else return false;
+			}
+			case 2:{//down
+				if(dx < 120 && dy > 160.0F)
+					return true;
+				else return false;
+			}
+			case 3:{//right
+				if(dx >360 && dy > 160.0F)
+					return true;
+				else return false;
+			}
+			case 4:{
+				if(dx>120 && dx<240 && dy<160)
+					return true;
+				else return false;
+			}
+			case 5:{
+				if(dx>120 && dx<240 && dy>160)
+					return true;
+				else return false;
+			}
+			case 6:{
+				if(dx>240 && dx<360 && dy<160)
+					return true;
+				else return false;
+			}
+			case 7:{
+				if(dx>240 && dx<360 && dy>160)
+					return true;
+				else return false;
+			}
+		}
 		}
 		return false;
 	}
@@ -152,46 +223,53 @@ public class Main extends Activity implements SensorEventListener {
 		
 	}
 	private void setButtons() {
-		final RelativeLayout lay = (RelativeLayout) findViewById(R.id.Controller_Layout);
+		final RelativeLayout layAceI = (RelativeLayout) findViewById(R.id.Controller_Layout_320_480);
+		final RelativeLayout layAceII = (RelativeLayout) findViewById(R.id.Controller_Layout);
+		final RelativeLayout lay = (ScreenAceII) ? layAceII : layAceI;
+
 		lay.setOnTouchListener(new View.OnTouchListener() {
 			@Override
 			public boolean onTouch(View v, MotionEvent event) {
 				switch(MotionEvent.ACTION_MASK & event.getAction()){
 					case MotionEvent.ACTION_DOWN:{
+//						System.out.print("\naction down\n");
+						mTextState.setText("action down");
 						float dx = event.getX();
 						float dy = event.getY();
 						
 						if(ClickedButton(dx, dy, 0)){//up
 							if(mConnectedThread!= null)
-								mConnectedThread.write(0x00);
+								mConnectedThread.write(global_UP);
 							lay.setBackgroundColor(Color.RED);
+						
 						}
 						else if(ClickedButton(dx, dy, 1)){//left
 							if(mConnectedThread!= null)
-								mConnectedThread.write(0x01);
+								mConnectedThread.write(global_LEFT);
 							lay.setBackgroundColor(Color.BLUE);
 						}
 						else if(ClickedButton(dx, dy, 2)){//down
 							if(mConnectedThread!= null)
-								mConnectedThread.write(0x02);
+								mConnectedThread.write(global_DOWN);
 							lay.setBackgroundColor(Color.YELLOW);
+						
 						}
 						else if(ClickedButton(dx, dy, 3)){//right
 							if(mConnectedThread!= null)
-								mConnectedThread.write(0x03);
+								mConnectedThread.write(global_RIGHT);
 							lay.setBackgroundColor(Color.BLACK);
 						}
 						else if(ClickedButton(dx, dy, 4)){//sound
 							if(mConnectedThread!= null)
-								mConnectedThread.write(0x16);
+								mConnectedThread.write(global_SOUND_ON);
 							lay.setBackgroundColor(Color.LTGRAY);
 							ImageView view = (ImageView)findViewById(R.id.HornButton);
 							view.setImageResource(R.drawable.a_sound_up);
-							
+						
 						}
 						else if(ClickedButton(dx, dy, 5)){//light
 							if(mConnectedThread!= null)
-								mConnectedThread.write(0x18);
+								mConnectedThread.write(global_SWITCH_LIGHTS);
 							lay.setBackgroundColor(Color.MAGENTA);
 							LIGHT = (LIGHT==true)? false: true;
 							ImageView view = (ImageView)findViewById(R.id.LightButton);
@@ -199,74 +277,169 @@ public class Main extends Activity implements SensorEventListener {
 								view.setImageResource(R.drawable.light_up);
 							else
 								view.setImageResource(R.drawable.light_down);
+						
 						}
 						else if(ClickedButton(dx, dy, 6)){//acc++
 							if(mConnectedThread!= null)
-								mConnectedThread.write(0x19);
+								mConnectedThread.write(global_INCREASE_SPEED);
 							lay.setBackgroundColor(Color.CYAN + 123);
 						}
 						else if(ClickedButton(dx, dy, 7)){//acc--
 							if(mConnectedThread!= null)
-								mConnectedThread.write(0x20);
+								mConnectedThread.write(global_DECREASE_SPEED);
 							lay.setBackgroundColor(Color.BLUE - 200);
 						}
+					
 						return true;
 					}
 					case MotionEvent.ACTION_POINTER_DOWN:{
+						mTextState.setText("action pointer down");
 						float dx0=event.getX(0);
 						float dy0=event.getY(0);
 						float dx1=event.getX(1);
 						float dy1=event.getY(1);
+						//up+increase speed
+						if(ClickedButton(dx0, dy0, 0) && ClickedButton(dx1, dy1, 6) ||
+								ClickedButton(dx0, dy0, 6) && ClickedButton(dx1, dy1, 0)){
+							if(mConnectedThread != null){
+								mConnectedThread.write(global_UP);
+								mConnectedThread.write(global_INCREASE_SPEED);
+							}
+							lay.setBackgroundColor(Color.MAGENTA-128);
+						
+						}
+						//up+decrease speed
+						if(ClickedButton(dx0, dy0, 0) && ClickedButton(dx1, dy1, 7) ||
+								ClickedButton(dx0, dy0, 7) && ClickedButton(dx1, dy1, 0)){
+							if(mConnectedThread != null){
+								mConnectedThread.write(global_UP);
+								mConnectedThread.write(global_DECREASE_SPEED);
+							}
+							lay.setBackgroundColor(Color.MAGENTA+128);
+						
+						}
+						//down+increase speed
+						if(ClickedButton(dx0, dy0, 2) && ClickedButton(dx1, dy1, 6) ||
+								ClickedButton(dx0, dy0, 6) && ClickedButton(dx1, dy1, 2)){
+							if(mConnectedThread != null){
+								mConnectedThread.write(global_UP);
+								mConnectedThread.write(global_INCREASE_SPEED);
+							}
+							lay.setBackgroundColor(Color.MAGENTA+128);
+						
+						}
+						//down+decrease speed
+						if(ClickedButton(dx0, dy0, 2) && ClickedButton(dx1, dy1, 7) ||
+								ClickedButton(dx0, dy0, 7) && ClickedButton(dx1, dy1, 2)){
+							if(mConnectedThread != null){
+								mConnectedThread.write(global_UP);
+								mConnectedThread.write(global_DECREASE_SPEED);
+							}
+							lay.setBackgroundColor(Color.MAGENTA-128);
+					
+						}
 						//up+left
 						if(ClickedButton(dx0, dy0, 0) && ClickedButton(dx1, dy1, 1) ||
 								ClickedButton(dx0, dy0, 1) && ClickedButton(dx1, dy1, 0)){
-							if(mConnectedThread != null)
-								mConnectedThread.write(0x09);
-							lay.setBackgroundColor(RandomColor());
+							if(mConnectedThread != null){
+								mConnectedThread.write(global_UP);
+								mConnectedThread.write(global_LEFT);
+							}
+							lay.setBackgroundColor(0xffa500);
+					
+							//sytuacja wyciśnięcia
 						}//up+right
 						else if(ClickedButton(dx0, dy0, 0) && ClickedButton(dx1, dy1, 3) ||
 								ClickedButton(dx0, dy0, 3) && ClickedButton(dx1, dy1, 0)){
-							if(mConnectedThread != null)
-								mConnectedThread.write(0x08);
-							lay.setBackgroundColor(RandomColor());
+							if(mConnectedThread != null){
+								mConnectedThread.write(global_RIGHT);
+								mConnectedThread.write(global_UP);
+							}
+							lay.setBackgroundColor(0xF20056);
+						
 						}//down+right
 						else if(ClickedButton(dx0, dy0, 2) && ClickedButton(dx1, dy1, 3) ||
 								ClickedButton(dx0, dy0, 3) && ClickedButton(dx1, dy1, 2)){
-							if(mConnectedThread != null)
-								mConnectedThread.write(0x10);
-							lay.setBackgroundColor(RandomColor());
+							if(mConnectedThread != null){
+								mConnectedThread.write(global_RIGHT);
+								mConnectedThread.write(global_DOWN);
+							}
+							lay.setBackgroundColor(0x336699);
+						
 						}//down+left
 						else if(ClickedButton(dx0, dy0, 2) && ClickedButton(dx1, dy1, 1) ||
 								ClickedButton(dx0, dy0, 1) && ClickedButton(dx1, dy1, 2)){
-							if(mConnectedThread != null)
-								mConnectedThread.write(0x11);
+							if(mConnectedThread != null){
+								mConnectedThread.write(global_LEFT);
+								mConnectedThread.write(global_DOWN);
+							}
+							lay.setBackgroundColor(0xCC9933);
+						
+						}
+						return true;
+					}
+					case MotionEvent.ACTION_POINTER_UP:{
+						mTextState.setText("action pointer up");
+						float dx0=event.getActionIndex();
+						float dy0=event.getActionIndex();
+						if(ClickedButton(dx0, dy0, 0)){
+							if(mConnectedThread != null){
+//								mConnectedThread.write(global_UP_DISABLE);
+								
+								mConnectedThread.write(global_RIGHT_DISABLE);
+								mConnectedThread.write(global_LEFT_DISABLE);
+								//mConnectedThread.write(global_UP);
+
+							}
+							lay.setBackgroundColor(RandomColor());
+						}
+						if(ClickedButton(dx0, dy0, 2)){
+							if(mConnectedThread != null){
+//								mConnectedThread.write(global_DOWN_DISABLE);;
+								mConnectedThread.write(global_RIGHT_DISABLE);
+								mConnectedThread.write(global_LEFT_DISABLE);
+								//mConnectedThread.write(global_DOWN);
+							}
+							lay.setBackgroundColor(RandomColor());
+						}
+						if(ClickedButton(dx0, dy0, 1)){
+							if(mConnectedThread != null){
+//								mConnectedThread.write(global_LEFT_DISABLE);
+								mConnectedThread.write(global_UP_DISABLE);
+								mConnectedThread.write(global_DOWN_DISABLE);
+								mConnectedThread.write(global_RIGHT_DISABLE);
+								mConnectedThread.write(global_LEFT);
+								
+							}
+							lay.setBackgroundColor(RandomColor());
+						}
+						if(ClickedButton(dx0, dy0, 3)){
+							if(mConnectedThread != null){
+								mConnectedThread.write(global_UP_DISABLE);
+								mConnectedThread.write(global_DOWN_DISABLE);
+								mConnectedThread.write(global_LEFT_DISABLE);
+								mConnectedThread.write(global_RIGHT);
+//								mConnectedThread.write(global_RIGHT_DISABLE);
+							}
 							lay.setBackgroundColor(RandomColor());
 						}
 						return true;
 					}
 					case MotionEvent.ACTION_UP:{
+						mTextState.setText("action up");
 						if(mConnectedThread != null){
-							//normal directions
-							mConnectedThread.write(0x04);
-							mConnectedThread.write(0x05);
-							mConnectedThread.write(0x06);
-							mConnectedThread.write(0x07);
-							//multitouch directions
-							mConnectedThread.write(0x12);
-							mConnectedThread.write(0x13);
-							mConnectedThread.write(0x14);
-							mConnectedThread.write(0x15);
-							//Sound disable
-							mConnectedThread.write(0x17);
+							mConnectedThread.write(global_UP_DISABLE);
+							mConnectedThread.write(global_LEFT_DISABLE);
+							mConnectedThread.write(global_RIGHT_DISABLE);
+							mConnectedThread.write(global_DOWN_DISABLE);
+							mConnectedThread.write(global_SOUND_OFF);
 						}
 						lay.setBackgroundColor(Color.DKGRAY);
 						ImageView view = (ImageView)findViewById(R.id.HornButton);
 						view.setImageResource(R.drawable.a_sound_down);
-						
 						return true;
 					}
 				}
-
 				return false;
 			}
 		});
@@ -431,9 +604,7 @@ public class Main extends Activity implements SensorEventListener {
 			break;
 
 		}
-
 	}
-
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
@@ -445,7 +616,8 @@ public class Main extends Activity implements SensorEventListener {
 
 		menu.findItem(R.id.connect).setVisible(STATE != CONNECTED);
 		menu.findItem(R.id.disconnect).setVisible(STATE == CONNECTED);
-
+		menu.findItem(R.id.item1).setVisible(ScreenAceII);
+		menu.findItem(R.id.item2).setVisible(!ScreenAceII);
 		return super.onPrepareOptionsMenu(menu);
 	}
 
@@ -459,6 +631,18 @@ public class Main extends Activity implements SensorEventListener {
 		case R.id.disconnect:
 			btDisconnect();
 			mDevice = null;
+			return true;
+		case R.id.item1:
+			ScreenAceII = false;
+			setContentView(R.layout.car_controller_layout_320_480);
+			setButtons();
+			return true;
+		
+		case R.id.item2:
+			ScreenAceII = true;
+			setContentView(R.layout.car_controller_layout);
+			setButtons();
+			
 			return true;
 		default:
 			return super.onOptionsItemSelected(item);
